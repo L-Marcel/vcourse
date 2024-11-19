04fad8c834ea63391b46374bd63204737f736feb
l-marcel <lmgh1312@gmail.com>
2024-11-19 10:51:11 -0300

    Removendo arquivos desnecessários e criando base estática da página de login (incompleta)

---
+++ src/components/ListItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <article
+    tabindex="0"
+    class="flex bg-zinc-200 p-4 hover:bg-green-400 group transition-colors duration-200 hover:cursor-pointer focus-visible:outline-none focus-visible:ring-2 ring-green-500"
+  >
+    <div class="flex flex-col gap-1">
+      <h3
+        class="font-medium text-xl bg-zinc-300 pr-1 w-min text-nowrap transition-colors duration-200 group-hover:bg-green-500"
+      >
+        <span
+          class="bg-green-500 px-1 mr-1 inline-block h-full transition-colors duration-200 group-hover:bg-green-600"
+          >0</span
+        >Introdução ao Vue.js
+      </h3>
+      <p>Aprenda os conceitos básicos dessa tecnologia.</p>
+      <div class="flex gap-2">
+        <time class="font-extralight" datetime="2024-11-19 20:00">19/11/2024</time>
+        <span class="font-extralight">|</span>
+        <time class="font-thin">2 horas e 3 minutos</time>
+      </div>
+    </div>
+  </article>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import ListItem from '../components/ListItem.vue'
+</script>
+
+<template>
+  <main class="p-8 min-h-screen flex flex-col gap-4">
+    <section class="flex flex-col gap-2">
+      <h1 class="w-min text-nowrap pr-1 font-semibold text-4xl bg-zinc-300">
+        <span class="bg-green-500 px-1 mr-1 inline-block h-full">V</span>Course
+      </h1>
+      <p class="">
+        O melhor curso gratuido de <span class="text-green-600 font-semibold">Vue.js</span> do
+        <span class="text-green-600 font-semibold">Brasil</span> inteiro!
+      </p>
+      <hr class="max-w-[75%]" />
+      <p class="font-light max-w-[75%]">
+        A cada semana liberamos 5 aulas novas e completamente inovadoras para que você e seu time
+        possa se manter atualizado sobre os recursos mais novos dessa incrível tecnologia, que é uma
+        das melhores do mercado! E as aulas que ficarem obsoletas serão substituídas ou removidas
+        periodicamente.
+      </p>
+      <p class="font-light">Prezamos a qualidade e simplicidade.</p>
+    </section>
+    <section class="flex flex-col w-full min-h-full h-full gap-4">
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+    </section>
+  </main>
+</template>
